3e2381eb399b4022fcd968b8035cbe4acf5620b6
wierzba3 <wierzba3@uwm.edu>
2015-02-01 18:00:09 -0700

    Add "AddLift" activity

--- app/src/main/java/wierzba/james/liftlog/AddLift.java
+++ app/src/main/java/wierzba/james/liftlog/AddLift.java
@@ -4,22 +4,25 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.content.Intent;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class MainActivity extends ActionBarActivity {
+
+public class AddLift extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_add_lift);
+
+        createContents();
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_add_lift, menu);
         return true;
     }
 
@@ -38,10 +41,12 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void startAddLift(View view){
-        Intent intent = new Intent(this, AddLift.class);
-
-        this.startActivity(intent);
+    private void createContents(){
+        Spinner spinner = (Spinner) findViewById(R.id.spn_exercise);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.arr_exercises, android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinner.setAdapter(adapter);
     }
 
 }
