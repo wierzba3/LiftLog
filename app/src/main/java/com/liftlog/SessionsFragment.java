bd0e46eafc7dbd4503e4ac6aa0ef12b64dc34ad0
wierzba3 <jamesmw129@gmail.com>
2015-10-15 15:01:24 -0600

    - change icon to be less bright - Put full month in Sessions list headers

--- app/src/main/java/com/liftlog/SessionsFragment.java
+++ app/src/main/java/com/liftlog/SessionsFragment.java
@@ -1,6 +1,7 @@
 package com.liftlog;
 
 import android.app.ActionBar;
+import android.app.ExpandableListActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -17,6 +18,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListView;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -29,6 +31,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.liftlog.common.Util;
 import com.liftlog.data.DataAccessObject;
 import com.liftlog.components.DateInputDialog;
 import com.liftlog.models.Exercise;
@@ -41,6 +44,9 @@ import org.joda.time.Months;
 
 public class SessionsFragment extends Fragment implements  DateInputDialog.DateInputDialogListener
 {
+
+    private ExpandableListView exListSessions;
+    private SessionsExpendableListAdapter exListAdapter;
 
     private ListView listSessions;
     private TextView lblEmpty;
@@ -107,6 +113,8 @@ public class SessionsFragment extends Fragment implements  DateInputDialog.DateI
             }
         });
 
+        exListSessions = (ExpandableListView) view.findViewById(R.id.exlist_sessions_fragment);
+
         FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab_add_session);
         fab.setOnClickListener(new View.OnClickListener()
         {
@@ -139,6 +147,7 @@ public class SessionsFragment extends Fragment implements  DateInputDialog.DateI
         {
             lblEmpty.setText("No sessions have been added");
             listSessions.setAdapter(null);
+            exListSessions.setAdapter((SessionsExpendableListAdapter)null);
             return;
         }
         lblEmpty.setText("");
@@ -146,18 +155,22 @@ public class SessionsFragment extends Fragment implements  DateInputDialog.DateI
         Session.computeDuplicateDays(sessions);
         Collections.sort(sessions, Session.byDateDesc);
 
-//        Session dummySession = new Session();
-//        dummySession.setId(-1);
-//        sessions.add(0, dummySession);
+//        ArrayAdapter<Session> adapter = new ArrayAdapter<Session>(super.getActivity(), android.R.layout.simple_list_item_1, sessions);
+//        listSessions.setAdapter(adapter);
 
-//            ArrayList<String> sessionLabels = new ArrayList<String>();
-//            for (Session session : sessions) sessionLabels.add(String.valueOf(session.getDate()));
-
-        ArrayAdapter<Session> adapter = new ArrayAdapter<Session>(super.getActivity(), android.R.layout.simple_list_item_1, sessions);
-
-        listSessions.setAdapter(adapter);
+        exListAdapter = new SessionsExpendableListAdapter(super.getActivity(), sessions);
+        exListSessions.setAdapter(exListAdapter);
+        expandListGroupItems();
     }
 
+    public void expandListGroupItems()
+    {
+        if(exListSessions == null || exListSessions == null) return;
+        for(int i = 0; i < exListAdapter.getGroupCount(); i++)
+        {
+            exListSessions.expandGroup(i);
+        }
+    }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
@@ -218,6 +231,9 @@ public class SessionsFragment extends Fragment implements  DateInputDialog.DateI
         loadSessions();
     }
 
+    /**
+     * Object to encapsulate the Group and Child elements of the Sessions ExpandableListView
+     */
     private class SessionGroupElement
     {
         public SessionGroupElement() {}
@@ -244,6 +260,26 @@ public class SessionsFragment extends Fragment implements  DateInputDialog.DateI
         {
             this.date = date;
         }
+
+//        @Override
+//        public boolean equals(Object o)
+//        {
+//            if(!(o instanceof SessionGroupElement)) return false;
+//            SessionGroupElement that = (SessionGroupElement)o;
+//            if(date == null && that.getDateTime() == null) return true;
+//            else if(date == null) return false;
+//            else if(that.getDateTime() == null) return false;
+//            return date.getYear() == that.getDateTime().getYear()
+//                    && date.getMonthOfYear() == that.getDateTime().getMonthOfYear();
+//        }
+//
+//        @Override
+//        public int hashCode()
+//        {
+//            if(date == null) return 0;
+//            return date.getYear() * 100 + date.getMonthOfYear();
+//        }
+
     }
 
     private class SessionsExpendableListAdapter extends BaseExpandableListAdapter
@@ -255,16 +291,61 @@ public class SessionsFragment extends Fragment implements  DateInputDialog.DateI
             if(allSessions == null) return;
 
 
+            for(Session session : allSessions)
+            {
+                if(session == null) continue;
+
+                DateTime dt = new DateTime(session.getDate());
+                SessionGroupElement element = findElementByMonth(dt);
+                if(element == null)
+                {
+                    element = new SessionGroupElement();
+                    element.setDateTime(dt);
+                    element.setSessions(new ArrayList<Session>());
+                    elements.add(element);
+                }
+
+                if(element.getSessions() == null)
+                {
+                    element.setSessions(new ArrayList<Session>());
+                }
+
+                element.getSessions().add(session);
+            }
+
+            for(SessionGroupElement element : elements)
+            {
+                if(element == null || element.getSessions() == null) continue;
+                Collections.sort(element.getSessions(), Session.byDateDesc);
+            }
 
             Collections.sort(elements, comparator);
         }
+
+        private SessionGroupElement findElementByMonth(DateTime findDt)
+        {
+            if(elements == null) return null;
+            for(SessionGroupElement element : elements)
+            {
+                DateTime dt = element.getDateTime();
+                if(dt == null) continue;
+                if(dt.getYear() == findDt.getYear() && dt.getMonthOfYear() == findDt.getMonthOfYear())
+                {
+                    return element;
+                }
+            }
+            return null;
+        }
+
 
         private Comparator<SessionGroupElement> comparator = new Comparator<SessionGroupElement>(){
             @Override
             public int compare(SessionGroupElement e1, SessionGroupElement e2)
             {
-                //TODO
-                return 0;
+                if(e1 == null && e2 == null) return 0;
+                else if(e1 == null) return 1;
+                else if(e2 == null) return -1;
+                return e2.getDateTime().compareTo(e1.getDateTime());
             }
         };
 
@@ -335,7 +416,11 @@ public class SessionsFragment extends Fragment implements  DateInputDialog.DateI
                 view = vi.inflate(R.layout.session_group_item, viewGroup, false);
             }
 
-            //TODO
+            TextView lblSessionGroup = (TextView)view.findViewById(R.id.lbl_session_group);
+
+            SessionGroupElement element = elements.get(i);
+
+            lblSessionGroup.setText(Util.DATE_FORMAT_MONTH.print(element.getDateTime()));
 
             return view;
         }
@@ -349,7 +434,30 @@ public class SessionsFragment extends Fragment implements  DateInputDialog.DateI
                 view = vi.inflate(R.layout.session_item, viewGroup, false);
             }
 
-            //TODO
+            TextView lblSession = (TextView) view.findViewById(R.id.lbl_session);
+            final Session session = elements.get(i).getSessions().get(j);
+            lblSession.setText(session == null ? "n/a" : session.toString());
+            lblSession.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View view)
+                {
+                    Intent intent = new Intent(SessionsFragment.super.getActivity(), ViewSessionActivity.class);
+                    long sessionId = session.getId();
+                    //if id is -1, the user selected the <New session> dummy item
+                    if (sessionId == -1)
+                    {
+                        doAdd();
+                        return;
+                    }
+
+                    view.setBackgroundColor(getResources().getColor(R.color.material_blue_200));
+//                view.setBackgroundResource(R.drawable.list_click_background);
+
+                    intent.putExtra(ViewSessionActivity.SESSION_ID_KEY, sessionId);
+                    startActivity(intent);
+                }
+            });
 
             return view;
         }
