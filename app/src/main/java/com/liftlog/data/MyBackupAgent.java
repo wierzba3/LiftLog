71c4558b28a6a68f070380beb7eeaa59447e458a
wierzba3 <jamesmw129@gmail.com>
2016-02-13 12:42:36 -0700

    - modify backup agent to backup the binary database file

--- app/src/main/java/com/liftlog/data/MyBackupAgent.java
+++ app/src/main/java/com/liftlog/data/MyBackupAgent.java
@@ -1,27 +1,27 @@
 package com.liftlog.data;
 
-import android.app.backup.BackupAgent;
-import android.app.backup.BackupDataInput;
-import android.app.backup.BackupDataOutput;
-import android.os.ParcelFileDescriptor;
+import android.app.backup.BackupAgentHelper;
+import android.app.backup.FileBackupHelper;
 
-import java.io.IOException;
+import java.io.File;
 
 /**
  * Created by James Wierzba on 10/13/2015.
  */
-public class MyBackupAgent extends BackupAgent
+public class MyBackupAgent extends BackupAgentHelper
 {
 
     @Override
-    public void onBackup(ParcelFileDescriptor parcelFileDescriptor, BackupDataOutput backupDataOutput, ParcelFileDescriptor parcelFileDescriptor1) throws IOException
+    public void onCreate()
     {
-
+        FileBackupHelper dbs = new FileBackupHelper(this, DataAccessObject.DB_NAME);
+        addHelper("dbs", dbs);
     }
 
     @Override
-    public void onRestore(BackupDataInput backupDataInput, int i, ParcelFileDescriptor parcelFileDescriptor) throws IOException
+    public File getFilesDir()
     {
-
+        File path = getDatabasePath(DataAccessObject.DB_NAME);
+        return path.getParentFile();
     }
 }
